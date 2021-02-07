d53eb6a72dddc6db0538103f53f7e755825db3af
kazu-work <78477743+kazu-work@users.noreply.github.com>
2021-02-07 09:03:01 +0200

    hah

--- js/boxes.js
+++ js/boxes.js
@@ -1,48 +1,43 @@
-var scene, camera, renderer, box, box2, box3;
+
+var scene, camera, renderer, box;
 
 init();
 
 function init() {
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xaaaaaa);
-  
-  const geometry = new THREE.BoxGeometry(1,1,1);
-  const material = new THREE.MeshStandardMaterial({color:new THREE.Color('skyblue')});
-  box = new THREE.Mesh(geometry, material);
-  scene.add(box);
-  box2 = new THREE.Mesh(geometry, material);
-  box2.position.x = -1.5;
-  scene.add(box2);
-  box3 = new THREE.Mesh(geometry, material);
-  box3.position.x = 1.5;
-  scene.add(box3);
-  
-  const light = new THREE.DirectionalLight();
-  light.position.set(0,1,2);
-  scene.add(light);
-  
-  camera = new THREE.PerspectiveCamera( 75, 426 / 240, 0.1, 1000 );
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xaaaaaa);
 
-  camera.position.z = 2;
-  
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-  window.addEventListener( 'resize', onResize, false);
-  
-  update();
+    const geometry = new THREE.BoxGeometry(1,1,1);
+    const material = new THREE.MeshStandardMaterial({color:new THREE.Color('skyblue')});
+    box = new THREE.Mesh(geometry, material);
+    scene.add(box);
+    box2 = new THREE.Mesh(geometry, material);
+    box2.position.x = -1.5;
+    scene.add(box2);
+    box3 = new THREE.Mesh(geometry, material);
+    box3.position.x = 1.5;
+    scene.add(box3);
+
+    const light = new THREE.DirectionalLight();
+    light.position.set(0,1,2);
+    scene.add(light);
+
+    camera = new THREE.PerspectiveCamera( 75, 426 / 240, 0.1, 1000 );
+
+    camera.position.z = 2;
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 426, 240 );
+    document.body.appendChild( renderer.domElement );
+
+
+    update();
 }
 
 function update(){
-  requestAnimationFrame( update );
-  box.rotation.y += 0.01;
-  box2.rotation.y -= 0.01;
-  box3.rotation.y -= 0.01;
-	renderer.render( scene, camera );
-}
-
-function onResize(){
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+    requestAnimationFrame( update );
+    box.rotation.y += 0.01;
+    box2.rotation.y -= 0.01;
+    box3.rotation.y -= 0.01;
+    renderer.render( scene, camera );
 }
---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+function runScriptOne() {
+    var scene, camera, renderer;
+
+    init();
+
+    function init(){
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        camera.position.set(0, 3, 10);
+
+        const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820);
+        scene.add(ambient);
+
+        const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+        light.position.set( 1, 10, 6);
+        scene.add(light);
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( 426, 240 );
+        document.body.appendChild( renderer.domElement );
+
+        const controls = new THREE.OrbitControls( camera, renderer.domElement );
+        controls.enableZoom = false;
+        controls.target.set(0,4,0);
+        controls.update();
+
+
+        //Add meshes here
+
+        const height = 0.4;
+        const geometry = new THREE.BoxGeometry(3, height, 0.9);
+        const material = new THREE.MeshLambertMaterial( { color: 0xdcbbc7});
+
+        const mesh = new THREE.Mesh(geometry, material);
+
+        for(let row=0; row<20; row++) {
+            let yPos = row * (height + 0.05);
+            let offset = -1;
+            for(let count=0; count<3; count++){
+                const block = mesh.clone();
+                if(row % 2){
+                    block.rotation.y = Math.PI/2;
+                    block.position.set(offset, yPos, 0);
+                } else {
+                    block.position.set(0, yPos, offset);
+                }
+                scene.add(block);
+                offset++;
+            }
+        }
+
+        window.addEventListener( 'resize', resize, false);
+
+        update();
+    }
+
+    function update(){
+        requestAnimationFrame( update );
+        renderer.render( scene, camera );
+    }
+
+    function resize(){
+        camera.aspect = 426 / 240;
+        camera.updateProjectionMatrix();
+        renderer.setSize( 426, 240 );
+    }
+}
